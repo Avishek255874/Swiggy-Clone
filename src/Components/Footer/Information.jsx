80ee219560f9e8553249351b989f6a493ecdd1d3
Avishek255874 <avisheksharma6290@gmail.com>
2024-09-10 17:04:33 +0530

    first commit

--- src/Components/Footer/Information.jsx
+++ src/Components/Footer/Information.jsx
@@ -5,14 +5,14 @@ const Information = () => {
   return (
     <div className="  flex justify-center bg-black h-[400px] text-white p-5 ">
       <div className="flex justify-between w-[1200px]">
-        <div className="border-2 border-white p-3">
+        <div className="p-3">
           <img src={swigg} alt="" className="w-[150px] h-[60px]" />
 
           <p className="text-slate-400 ps-3">©️2024 ASSASS Pvt.Ltd Group. </p>
         </div>
-        <div className="border-2 border-white">hllooo</div>
-        <div className="border-2 border-white">jguugfghf</div>
-        <div className="border-2 border-white">jguugfghf</div>
+        <div className="">hllooo</div>
+        <div className="">jguugfghf</div>
+        <div className="">jguugfghf</div>
       </div>
     </div>
   );
